464182dac6dbb6706d8f279343f3070f030bf6f6
Mr.Li <57608648+USER-FuX@users.noreply.github.com>
2020-04-24 09:56:21 +0800

    测试

---
+++ HelloNetty/src/main/java/xyz/fuxnet/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package xyz.fuxnet.test;
+
+/**
+ * 功能描述: <br>
+ *
+ * @author FuX
+ * @CalssName xyz.fuxnet.test.HelloWorld
+ * @Date 2020/4/24 9:55
+ * @Version V1.0
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World!!!!");
+    }
+}
