f818a373f04080699658337c6d5f386d999a0a4c
Mr.U <57608648+USER-FuX@users.noreply.github.com>
2020-06-11 10:30:58 +0800

    null

--- HelloNetty/src/main/java/xyz/fuxnet/test/HelloWorld.java
+++ HelloNetty/src/main/java/xyz/fuxnet/test/HelloWorld.java
@@ -11,6 +11,6 @@ package xyz.fuxnet.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World!!!!");
+        System.out.println("Hello World!!!!");
     }
 }
