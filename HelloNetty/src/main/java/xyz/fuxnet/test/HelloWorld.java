01fda3927e78defa3bb1008ce0f8c242f094e5a7
Mr.U <57608648+USER-FuX@users.noreply.github.com>
2020-06-21 19:59:19 +0800

    pom文件配置JDK版本

--- HelloNetty/src/main/java/xyz/fuxnet/test/HelloWorld.java
+++ HelloNetty/src/main/java/xyz/fuxnet/test/HelloWorld.java
@@ -11,6 +11,29 @@ package xyz.fuxnet.test;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World!!!!");
+
+    }
+
+    /**
+     * 功能描述: <br>
+     * 〈冒泡排序〉
+     *
+     * @Param: [arr]
+     * @Return: int[]
+     * @Author: FuX
+     * @Date: 2020/6/21 15:47
+     * @Update:
+     */
+    private static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j + 1] > arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
